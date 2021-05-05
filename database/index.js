f1eba4c0072177cb33793238cf0c5f366929dab7
zubair-akbar <62559732+zubair-akbar@users.noreply.github.com>
2021-05-04 23:12:37 -0400

    Database connection complete

--- database/index.js
+++ database/index.js
@@ -1,1 +1,32 @@
 const { user, password, database } = require('./config');
+const { Client } = require('pg');
+const pgp = require('pg-promise')
+
+const client = new Client({
+  host: 'localhost',
+  user: `${user}`,
+  password: `${password}`,
+  database: `${database}`,
+  port: 5432,
+  max: 20,
+});
+
+client.connect((err) => {
+  if (err) {
+    console.error('connection error: ', err.stack);
+  } else {
+    console.log('connected to postgres database pool')
+  }
+});
+
+//var connectionString = `postgres://${user}:${password}@localhost:5432/${database}`;
+
+// const db=pgp(connectionString)
+
+// async function testConnection() {
+//   const c = await db.connect();
+//   c.done();
+//   return c.client.serverVersion;
+// }
+
+module.exports = client;
