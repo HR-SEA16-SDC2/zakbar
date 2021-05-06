4cde31719bd78aaa5c8e6aa4542ef14cd7a28dbf
zubair-akbar <62559732+zubair-akbar@users.noreply.github.com>
2021-05-06 19:00:15 -0400

    Functional schema holding CSV data

--- database/schema.sql
+++ database/schema.sql
@@ -7,26 +7,28 @@ USE qanda;
 CREATE TABLE questions (
   question_id SERIAL NOT NULL,
   product_id INTEGER NOT NULL,
-  body VARCHAR (75) NOT NULL,
+  body VARCHAR (250) NOT NULL,
   date_written DATE NOT NULL,
-  asker_name VARCHAR(20) NOT NULL,
-  asker_email VARCHAR(20) NOT NULL,
+  asker_name VARCHAR(250) NOT NULL,
+  asker_email VARCHAR(250) NOT NULL,
   reported BOOLEAN,
   helpful INTEGER,
-  PRIMARY KEY (product_id)
+  PRIMARY KEY (question_id),
+  UNIQUE(question_id)
 );
 
 CREATE TABLE answers (
   answer_id  SERIAL NOT NULL,
   question_id INTEGER NOT NULL,
-  body VARCHAR (75) NOT NULL,
+  body VARCHAR (250) NOT NULL,
   date_written DATE NOT NULL,
-  answerer_name VARCHAR(20) NOT NULL,
-  answerer_email VARCHAR(20) NOT NULL,
+  answerer_name VARCHAR(250) NOT NULL,
+  answerer_email VARCHAR(250) NOT NULL,
   reported BOOLEAN,
   helpful INTEGER,
   PRIMARY KEY (answer_id),
-  FOREIGN KEY (question_id) REFERENCES questions(question_id)
+  FOREIGN KEY (question_id) REFERENCES questions(question_id),
+  UNIQUE(answer_id)
 );
 
 CREATE TABLE photos (
@@ -34,7 +36,6 @@ CREATE TABLE photos (
   answer_id INTEGER NOT NULL,
   url VARCHAR (2048) NOT NULL,
   PRIMARY KEY (photo_id),
-  FOREIGN KEY (answer_id) REFERENCES answers(answer_id)
+  FOREIGN KEY (answer_id) REFERENCES answers(answer_id),
+  UNIQUE(photo_id)
 );
-
---serial
