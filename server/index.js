44568ae08c3287ee5bf532d674979e0820a8d432
zubair-akbar <62559732+zubair-akbar@users.noreply.github.com>
2021-05-07 00:31:39 -0400

    Continued updates to server, asynch implementation

--- server/index.js
+++ server/index.js
@@ -1,17 +1,24 @@
-// const express = require('express');
-// const axios = require('axios');
-// const path = require('path');
-// const testConnection = require('../database/index.js');
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const morgan = require("morgan");
+const queries = require("../database/queries");
 
-// const app = express();
-// const PORT = 3000;
-// //const apiUrl = /*to AWS instance*/
-// //const header = { Authorization: config.TOKEN };
+//Middleware
+app.use(morgan("tiny"));
+app.use(express.json());
 
-// app.use(express.json());
+app.get("/qa/questions/${productId}", async function(req, res) {
+	try {
+		const allQuestions = await queries.getAllQuestions();
+		res.status(200).send(allQuestions);
+	} catch (e) {
+		console.error(e);
+		res.status(500).send(e);
+	}
+});
 
-// app.listen(PORT, () => {
-//   console.log(`Server is running and listening on port ${PORT}`);
-// });
 
-
+app.listen(PORT, () => {
+	console.log(`Server is running and listening on port ${PORT}`);
+});
