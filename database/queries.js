5d5cebc0878e5a71d5c14e6db00e09b6d64875fa
zubair-akbar <62559732+zubair-akbar@users.noreply.github.com>
2021-05-07 01:12:06 -0400

    Successful initial implementation of async await

--- database/queries.js
+++ database/queries.js
@@ -1,23 +1,58 @@
 const db = require(".");
-
+const Cursor = require("pg-cursor");
+//use cursor to pass large queries
 
 const query = `
 SELECT *
 FROM questions
-LIMIT 20
+LIMIT 2
 `;
 
-db.query(query, (err, res) => {
-	if (err) {
-		console.error(err);
-		return;
-	}
-	for (let row of res.rows) {
-		console.log(row);
-	}
-	db.end();
-});
+(async () => {
+	try {
+		const client = await db.connect();
+		const res = await client.query(query);
 
+		for (let row of res.rows) {
+			console.log(row);
+		}
+	} catch (err) {
+		console.error(err);
+	}
+})();
+
+
+
+
+
+//Functional  error first statement to retrieve query
+// db.query(query, (err, res) => {
+// 	if (err) {
+// 		console.error(err);
+// 		return;
+// 	}
+// 	for (let row of res.rows) {
+// 		console.log(row);
+// 	}
+// 	db.end();
+// });
+
+//Promise based connection to pool
+// pool.connect()
+//     .then((client) => {
+//         client.query(query)
+//             .then(res => {
+//                 for (let row of res.rows) {
+//                     console.log(row);
+//                 }
+//             })
+//             .catch(err => {
+//                 console.error(err);
+//             });
+//     })
+//     .catch(err => {
+//         console.error(err);
+//     });
 
 // const controllers = {
 
